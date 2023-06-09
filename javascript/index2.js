759a9af468ec3f20fe71806c4b4d3816d9b7522b
Parth Madhvani <95546695+ParthMadhvani2@users.noreply.github.com>
2023-06-09 21:37:04 +0530

    Add files via upload

---
+++ javascript/index2.js
@@ -0,0 +1,28 @@
+console.log('lets start');
+
+const rectangle ={
+    lenght : 1,
+    breadth : 2,
+
+    draw: function(){
+        console.log('drawing rectangle');
+    }
+};
+
+const rectangle1 ={
+    lenght : 1,
+    breadth : 2,
+
+    draw: function(){
+        console.log('drawing rectangle');
+    }
+};
+
+const rectangle2 ={
+    lenght : 1,
+    breadth : 2,
+
+    draw: function(){
+        console.log('drawing rectangle');
+    }
+};
