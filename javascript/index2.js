673b12c20aa7a33dc13b24ccf535114828d8c346
Parth Madhvani <95546695+ParthMadhvani2@users.noreply.github.com>
2023-06-11 23:00:10 +0530

    Update index2.js

--- javascript/index2.js
+++ javascript/index2.js
@@ -1,28 +1,92 @@
-console.log('lets start');
+console.log('Chaliye shuru karte hai ');
 
-const rectangle ={
-    lenght : 1,
-    breadth : 2,
+//object create
+// let rectangle = {
+//     length: 1,
+//     breadth: 2,
 
-    draw: function(){
-        console.log('drawing rectangle');
-    }
-};
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
 
-const rectangle1 ={
-    lenght : 1,
-    breadth : 2,
+//factory function 
 
-    draw: function(){
-        console.log('drawing rectangle');
-    }
-};
+// function createRectangle(len, bre) {
 
-const rectangle2 ={
-    lenght : 1,
-    breadth : 2,
+//     const rectangle = {
+//         length: len,
+//         breadth:bre ,
 
-    draw: function(){
-        console.log('drawing rectangle');
-    }
-};
+//         draw() {
+//             console.log('drawing rectangle');
+//         }
+// //     };
+//     return rectangle;
+// }
+
+// let rectangleObj1 = createRectangle(5, 4);
+// let rectangle2 = createRectangle(2,3);
+// let rectangle3 = createRectangle(7,9);
+
+//Camelcase -> numberOfStudents
+//constructor function -> Pascal Notation -> first letter of every word is Capital -> NumberOfStudents
+//constructor function -> prop/methods -> intialise/Define
+// function Rectangle(len, bre) {
+//     this.length = len;
+//     this.breadth = bre;
+//     this.draw= function() {
+//         console.log('drawing');
+//     }
+// }
+
+// Rectangle.
+
+// //object creation using constrcutor function
+// let rectangleObject = new Rectangle(4,6);
+
+// rectangleObject.color = 'yellow';
+// console.log(rectangleObject);
+
+// delete rectangleObject.color;
+// console.log(rectangleObject);
+
+// let Rectangle1 = new Function(
+//     'length', 'breadth',
+// `this.length = length;
+// this.breadth = breadth;
+// this.draw= function() {
+//     console.log('drawing');
+// }`);
+
+// //object creation using Rectangle1
+// let rect = new Rectangle1(2,3);
+
+// rect.length;
+
+// console.log(rect);
+
+
+// rectangle.length;
+// rectangle.breadth;
+
+// rectangle.draw();
+
+// let rectangle1 = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
+
+// let rectangle2 = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
