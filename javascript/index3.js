e8d9213491aa31e5d6867833210e1570e0e474b3
Parth Madhvani <madhvaniparth2@gmail.com>
2023-06-26 23:06:05 +0530

    javascript

---
+++ javascript/index3.js
@@ -0,0 +1,14 @@
+console.log('Hello Jee');
+
+let LastName = 'Madhvani';
+
+let firstName = new String ('Parth');
+
+let message = 'This is my \n first \n message';
+
+console.log(message);
+
+let.words = message.split(' ');
+
+console.log(words);
+
