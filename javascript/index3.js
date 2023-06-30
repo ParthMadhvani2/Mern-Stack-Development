ef1fbcdcfc4cc55e64a27236ca97fd8269085eac
Parth Madhvani <madhvaniparth2@gmail.com>
2023-06-30 23:03:04 +0530

    update

--- javascript/index3.js
+++ javascript/index3.js
@@ -5,17 +5,31 @@ let LastName = 'Madhvani';
 let firstName = new String ('Parth');
 
 // let message = 'This is my \n first \n message';
-let message = `
-Hello $(LastName),
+// let message = `
+// Hello $(LastName),
 
-thanks for the opportunity
+// thanks for the opportunity
 
-Regards,
-Parth`;
+// Regards,
+// Parth`;
 
-console.log(message);
+// console.log(message);
 
-let.words = message.split(' ');
+// let.words = message.split(' ');
 
-console.log(words);
+// console.log(words);
 
+
+// ways to print dates with javascript 
+let date = new Date();
+let date2 = new Date('June 20 1998 07:15');
+let date3 = new Date(1998, 11, 20, 7);// here the month starts from 0 - 11 jan - Dec 
+
+console.log(date3);
+
+
+//Array in JS
+let numbers = [1, 2, 3, 4, 5, 6, 7];//declaration of arrays
+
+//print array in JS
+console.log(numbers);
