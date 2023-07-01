02142d225a41a9c5e21015a3d3029097680eb2bc
Parth Madhvani <madhvaniparth2@gmail.com>
2023-07-01 18:12:20 +0530

    update

--- javascript/index3.js
+++ javascript/index3.js
@@ -33,3 +33,49 @@ let numbers = [1, 2, 3, 4, 5, 6, 7];//declaration of arrays
 
 //print array in JS
 console.log(numbers);
+
+// //insertion in JS at end of array -> push
+// numbers.push(9);
+// console.log(numbers);
+
+// //insertion in JS at beginning of array -> unshift
+// numbers.unshift(0);
+// console.log(numbers);
+
+// // insertion in JS at end of array -> splice
+// numbers.splice(4, 0,3.5,3.6,3.7,3.8 ,3.9);  // .splice(at which index number,what to delete, what to add to array)
+// console.log(numbers);
+
+
+//searching
+// console.log(numbers);
+
+// console.log(numbers.indexOf(3));
+// console.log(numbers.indexOf(8));
+
+
+//we want to search that a number exists in an array?
+// if(numbers.indexOf(4)!=-1)
+// console.log("present number in array");       //this is not right way
+
+// console.log(numbers.includes(7));                //this is right way for searching for numbers
+
+// console.log(numbers.indexOf(4, 2));
+
+//array of object
+let courses = [
+    {no:1, naam:'Parth'},
+    {no:2, naam:'Tirth'}
+];
+
+console.log(courses);
+// both methods are wrong, as both have differnt addresses so values doesn't match so result is -1 and false
+// console.log(courses.indexOf({no:1, naam:'Parth'}));
+// console.log(courses.includes({no:1, naam:'Parth'}));
+
+// but, by using call by fuction we can find
+let course = courses.find(function (course){
+    return course.naam === 'Parth';
+})
+
+console.log(course);
