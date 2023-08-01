03e24cd60a231d06b5126d3fb996ae6d31a18235
Parth Madhvani <madhvaniparth2@gmail.com>
2023-08-01 23:09:57 +0530

    update

--- javascript-DOM/index3.js
+++ javascript-DOM/index3.js
@@ -1,20 +1,52 @@
-//ADDING 100 para
-//for loop
-for(let i=1; i<=100; i++){
-    let newElement = document.createElement('p');
-    newElement.textContent = 'This is para' + i;
+// //ADDING 100 para
+// //for loop
+// for(let i=1; i<=100; i++){
+//     let newElement = document.createElement('p');
+//     newElement.textContent = 'This is para' + i;
 
-    document.body.appendChild(newElement);
-}
+//     document.body.appendChild(newElement);
+// }
 
-//obtimizing a bit
-let myDiv = document.createElement('div');
+// //obtimizing a bit
+// let myDiv = document.createElement('div');
 
-for(let i=1; i<=100; i++){
-    let element = document.createElement('p');
-    element.textContent = 'This is para' + i;
+// for(let i=1; i<=100; i++){
+//     let element = document.createElement('p');
+//     element.textContent = 'This is para' + i;
 
-    myDiv.appendChild(element);
-}
+//     myDiv.appendChild(element);
+// }
 
-document.body.appendChild(myDiv);
+// document.body.appendChild(myDiv);
+// const t4 = performance.now();
+// console.log("this took " + (t4-t3) + " ms");
+
+
+// let fragment = document.createDocumentFragment();
+// for(let i=1; i<=100; i++) {
+//     let newElement = document.createElement('p');
+//     newElement.textContent = 'This is Para ' + i;
+
+//     fragment.appendChild(newElement);
+// }
+// document.body.appendChild(fragment); // 1 Reflow, 1 Repaint
+
+
+// function addPara() {
+//     let para = document.createElement('p');
+//     para.textContent = 'Js is Single';
+//     document.body.appendChild(para);
+// }
+
+// function appendNewMessage() {
+//     let para = document.createElement('p');
+//     para.textContent = 'Kya haal Chaal';
+//     document.body.appendChild(para);
+// }
+
+// addPara();
+// appendNewMessage();
+
+// document.addEventListener('click', function() {
+//     console.log('hello DiDi');
+// });
