f7d58e7c3fe071b16f6f8f63e91896b73183f5a6
Parth Madhvani <madhvaniparth2@gmail.com>
2023-08-02 23:40:17 +0530

    update

---
+++ mini-projects/psw-generator/script.js
@@ -0,0 +1,74 @@
+const inputSlider = document.querySelector("[data-length-slider]"); // to use custom attributes we use document.querySelector("[]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+const copyBtn = document.querySelector("[data-copy]");
+const copyMsg = document.querySelector("[data-copyMsg]");
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numbersCheck = document.querySelector("#number");
+const symbolsCheck = document.querySelector("#symbol");
+const indicator = document.querySelector("[data-indicator]");
+const generateBtn = document.querySelector(".generateBtn");
+const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+const symbols = '~`!@#$%^&*()_-+=[]{}|;:"<>?/.,';
+
+let password = "";
+let passwordLength = 10;
+let checkCount = 1;
+handleSlider();
+//set strength cicle color = grey;
+
+// set passwordLength 
+function handleSlider(){
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+}
+
+function setIndicator(color){
+    indicator.style.backgroundColor = color;
+    //shadow
+}
+
+function getRandomInteger(min, max){
+    Math.floor(Math.random() * (max - min)) + min;
+}
+
+function genrateRandomNumber(){
+    return getRandomInteger(0,9);
+}
+
+function generateLowerCase(){
+    return String.fromCharCode(getRandomInteger(97,123));
+}
+
+function generateUpperCase(){
+    return String.fromCharCode(getRandomInteger(65,91));
+}
+
+function generateSymbol(){
+    const ranNum = getRandomInteger(0, symbols.length);
+    return symbols.charAt(ranNum);
+}
+
+function calcStrength() {
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolsCheck.checked) hasSym = true;
+  
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+      setIndicator("#0f0");
+    } else if (
+      (hasLower || hasUpper) &&
+      (hasNum || hasSym) &&
+      passwordLength >= 6
+    ) {
+      setIndicator("#ff0");
+    } else {
+      setIndicator("#f00");
+    }
+}
