db753fc5f47dc9f4df6c6c6432c9d519e6589ad6
Parth Madhvani <madhvaniparth2@gmail.com>
2023-08-12 23:49:59 +0530

    update

--- mini-projects/weather-app/index.js
+++ mini-projects/weather-app/index.js
@@ -1,18 +1,111 @@
-console.log("Hello parth");
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
 
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+
+// Initiall need 
+let oldTab = userTab;
 const API_KEY = "ef37f22e82d102725e59e33fb573c214";
+oldTab.classList.add("current-tab");
+getfromSessionStorage();
+//what is panding
+// function switchTab(clickedTab){
+//     if(clickedTab != currentTab){
+//         currentTab.classList.remove("current-tab");
+//         currentTab = clickedTab;
+//         currentTab.classList.add("current-tab");
+        
+//         if(searchForm.classList.contains("active")){
+//                  //kya search form wala container is invisible, if yes then make it visible
+//             userInfoContainer.classList.remove("active");
+//             grantAccessContainer.classList.remove("active");
+//             searchForm.classList.add("active");
+//         }
+//         else{
+//             //main pehle search wale tab par tha , ab your weather tab par hu
+//             searchForm.classList.remove("active");
+//             userInfoContainer.classList.remove("active");
+//             //ab main your weather tab me aagya hu, toh weather bhi display karna poadega, so let's check local storage first
+//             //for coordinates, if we haved saved them there.
+//             getfromSetionStorege();
+//         }
+//     }
+// }
 
-async function showWeather(){
-    // let latitude = 15.3333; 
-    // let logitude = 74.0833;
-    let city = "goa";
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-    const data = await response.json();
 
-    console.log("Weather data :-> " , data);
+function switchTab(newTab) {
+    if(newTab != oldTab) {
+        oldTab.classList.remove("current-tab");
+        oldTab = newTab;
+        oldTab.classList.add("current-tab");
 
-    // let newPara = document.newElement('p');
-    // newPara.textContent = `${data?.main?.temp.toFixed(2)} C`
-
-    // document.body.appendChild(newPara);
+        if(!searchForm.classList.contains("active")) {
+            //kya search form wala container is invisible, if yes then make it visible
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+        }
+        else {
+            //main pehle search wale tab pr tha, ab your weather tab visible karna h 
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            //ab main your weather tab me aagya hu, toh weather bhi display karna poadega, so let's check local storage first
+            //for coordinates, if we haved saved them there.
+            getfromSessionStorage();
+        }
+    }
 }
+
+userTab.addEventListener("click", () => {
+    //pass clicked tab as input paramter
+    switchTab(userTab);
+});
+
+searchTab.addEventListener("click", () => {
+    //pass clicked tab as input paramter
+    switchTab(searchTab);
+});
+
+// if coordinates are already present in session storage 
+function getfromSessionStorage(){
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+    if(localCoordinates){
+        //agar localCoordinates nahi mile
+        grantAccessContainer.classList.add("active");
+    }
+    else{
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+}
+
+async function fetchUserWeatherInfo(coordinates){
+    const {lat, lon} = coordinates;
+    // make grantcontainer invisible
+    grantAccessContainer.classList.remove("active");
+    // make loader visible
+    loadingScreen.classList.add("active");
+
+    //API call
+    try{
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+          );
+        const  data = await response.json();
+
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(err){
+        loadingScreen.classList.remove("active");
+        console.log("Errror Found" , err);
+    }
+}
+
+function 
