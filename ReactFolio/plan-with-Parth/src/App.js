2114efd69a3013113b1f02b8e054350ada344e5d
Parth Madhvani <madhvaniparth2@gmail.com>
2023-12-16 22:13:12 +0530

    update

--- ReactFolio/plan-with-Parth/src/App.js
+++ ReactFolio/plan-with-Parth/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h2>Play with Parth</h2>
+      <Tours>
+
+      </Tours>
+    </div>
+  )
 };
 
 export default App;
