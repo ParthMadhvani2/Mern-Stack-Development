55bdf7874a3f7e299b4bc070746c1d8d9a90ab13
Parth Madhvani <madhvaniparth2@gmail.com>
2023-12-17 23:35:26 +0530

    update

---
+++ ReactFolio/plan-with-Parth/components/Card.js
@@ -0,0 +1,17 @@
+
+function Card(id, image, info, price, name){
+    return(
+        <div className="card">
+            <img src={image} alt="" className="image"></img>
+            <div className="tour-details">
+                <h4 className="tour-price">{price}</h4>
+                <h4 className="tour-name">{name}</h4>
+            </div>
+            <div className="description">
+                {description}
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- ReactFolio/plan-with-Parth/components/Tours.js
+++ ReactFolio/plan-with-Parth/components/Tours.js
@@ -1,11 +1,15 @@
-function Tours() {
+function Tours({tours}) {
     return(
         <div>
             <div>
                 <h2>Plan with Parth</h2>
             </div>
             <div>
-                
+                {
+                    tours.map( (tour) => {
+                        return <card {...tour}></card>
+                    })
+                }
             </div>
         </div>
     );
