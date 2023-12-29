abd3ad1e838b15ac58467e012f23b598207b58b6
Parth Madhvani <madhvaniparth2@gmail.com>
2023-12-29 22:10:57 +0530

    update

---
+++ top-course-react-app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar =() => {
+    return(
+        <div>
+            <nav>
+                <h1>Top Cources</h1>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
