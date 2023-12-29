41a4edbf795ca97f1987db758991b3246ab85c32
Parth Madhvani <madhvaniparth2@gmail.com>
2023-12-29 21:10:21 +0530

    update

--- Reactfolio2/dummy/src/App.js
+++ Reactfolio2/dummy/src/App.js
@@ -1,9 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const[text,setText] = useState('');
+  const [name,setName] = useState('Parth');
+
+  // variation 1 -> Every render
+  // useEffect(() => {
+  //   console.log("UI rendering done")
+  // });
+
+
+  // variation 2 ->
+  // useEffect(() =>{
+  //   console.log("UI Rendering done")
+  // },[]);
+
+
+  // variation 3 -> on first render + whenever dependency changes
+  // useEffect(() => {
+  //   console.log("Change Observed")
+  // },[name]);
+
+
+  // variation 4 -> to handle unmounting of a component
+  useEffect(() => {
+    // add event listner
+    console.log("Listner Add")
+
+    return () => {
+      console.log("Listner removed");
+    } 
+  },[text]);
   function changeHandler(event){
     console.log(text);
     setText(event.target.value)
