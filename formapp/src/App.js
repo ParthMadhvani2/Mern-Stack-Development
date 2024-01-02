4edc38ecca69e3f2bec4e980f1000e37fe8a3622
Parth Madhvani <madhvaniparth2@gmail.com>
2024-01-02 22:55:09 +0530

    update

--- formapp/src/App.js
+++ formapp/src/App.js
@@ -1,16 +1,54 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-    function changeHandler(event){
-      console.log(event.target.value);
-    }
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState("");
 
+    // console.log(firstName);
+    // console.log(lastName);
+    // function changeFirstNameHandler(event){
+    //   // console.log("printing first name");
+    //   // console.log(event.target.value);
+    //   setFirstName(event.target.value);
+    // }
+    // function changeLastNameHandler(event){
+    //   // console.log("printing last name");
+    //   // console.log(event.target.value);
+    //   setLastName(event.target.value);
+    // }
+
+    // // create an object
+  const[formData, setFormData] = useState ({firstName:"",lastName:"",email:""});
+  
+  function changeHandler(event){
+    setFormData(prevFormData => {
+     return{
+      ...prevFormData,
+      [event.target.name] : event.target.value
+    }
+    });
+  }
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder='name' onChange={changeHandler}/>
 
+        <br/>
+        <br/>
+
+        <input type="text" placeholder='first name' onChange={changeHandler} name="firstName" value={formData.firstName}/>
+        
+        <br/>
+        <br/>
+
+        <input type="text" placeholder='last name' onChange={changeHandler} name="lastName"  value={formData.lastName}/>
+      
+        <br/>
+        <br/>
+        <input type="email" placeholder='Enter your email here' onChange={changeHandler} name="email"  value={formData.email}/>
+      
+      
       </form>
     </div>
   );
