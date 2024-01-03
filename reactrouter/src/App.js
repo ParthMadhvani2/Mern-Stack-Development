b31da5009c8546d849e9c615110f706537f5a9b8
Parth Madhvani <madhvaniparth2@gmail.com>
2024-01-03 23:15:40 +0530

    update

---
+++ reactrouter/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {Route, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>}/>
+        <Route path="/support" element={<div>Support Page</div>}/>
+        <Route path="/about" element={<div>About Page</div>}/>
+        <Route path="/labs" element={<div>Labs Page</div>}/>
+        <Route path="*" element={<div>Not found page</div>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
