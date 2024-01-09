6604803c4405a5d4701b5467b057905924b87194
Parth Madhvani <madhvaniparth2@gmail.com>
2024-01-09 18:48:57 +0530

    update

--- blogs-context/src/App.js
+++ blogs-context/src/App.js
@@ -1,23 +1,25 @@
+import "./App.css";
+import { useContext, useEffect } from "react";
+import { AppContext } from "./context/AppContext";
 import Header from "./components/Header";
 import Blogs from "./components/Blogs";
 import Pagination from "./components/Pagination";
-import { useContext, useEffect } from "react";
-import { AppContext } from "./context/AppContext";
-import "./App.css"
-
 export default function App() {
-  const {fetchBlogPosts} = useContext(AppContext);
+  const { fetchBlogPosts } = useContext(AppContext);
 
   useEffect(() => {
+    // Fetch the inital Blogposts data
     fetchBlogPosts();
-  },[]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+    <>
       <Header />
-      <Blogs />
-      <Pagination />
-    </div>
+      <div className="my-[100px]">
+        <Blogs />
+        <Pagination />
+      </div>
+    </>
   );
-
 }
