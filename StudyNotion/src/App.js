34648d8f6681ac5831f19d45f2604525695f3952
Parth Madhvani <madhvaniparth2@gmail.com>
2024-01-21 15:49:30 +0530

    update

--- StudyNotion/src/App.js
+++ StudyNotion/src/App.js
@@ -1,40 +1,34 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Dashboard from "./pages/Dashboard"
-import { useEffect, useState } from 'react'
-import PrivateRoute from "./components/PrivateRoute";
-
+import Navbar from "./Components/Navbar";
+import Login from "./Components/Login";
+import Home from "./Components/Home.jsx";
+import Signup from "./Components/Signup";
+import Dashboard from "./Components/Dashboard";
+import PrivateRoute from "./Components/PrivateRoute";
+import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 
 function App() {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
- 
-
-  
-
   return (
-    <div className="w-screen h-screen bg-richblack-900 flex flex-col">
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
-
+    <div className="w-screen h-screen bg-richblack-900 flex flex-col ">
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
-
-        <Route path="/" element= {<Home />} setIsLoggedIn={setIsLoggedIn} />
-        <Route path="/login" element = {<Login  setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/signup" element={<Signup  setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/dashboard" element = {
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={<Login setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/signup"
+          element={<Signup setIsLoggedIn={setIsLoggedIn} />}
+        />
         <PrivateRoute isLoggedIn={isLoggedIn}>
-          <Dashboard/>
+          <Route path="/dashboard" element={<Dashboard />} />
         </PrivateRoute>
-        } />
-
       </Routes>
-
     </div>
-    )
+  );
 }
 
 export default App;
--- formapp/src/App.js
+++ formapp/src/App.js
@@ -1,107 +1,157 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
 
-  //useState
+  // function changeFirstNameHandler(event) {
+  //   // console.log(event.target);
+  //   console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // }
+
+  // function changeLastNameHandler(event) {
+  //   // console.log(event.target);
+  //   console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // }
+
   const [formData, setFormData] = useState({
-    firstName:"", lastName:"", email:"", country:"India", streetAddress:"", city:"", state:"", postalCode:"", 
-  })
+    firstName: "",
+    lastName: "",
+    email: "",
+    comment: "",
+    isAgree: true,
+    mode: "",
+    favCar: ""
+  });
 
-  //function for checking the check box 
+  // console.log(formData);
+
   function changeHandler(event) {
-    const {name,value,checked,type} = event.target;
-    setFormData((prev) => ({...prev, [name]:type === "checkbox" ? checked: value}) );
+    const { name, value, checked, type } = event.target;
+    setFormData((prevFormData) => {
+      // console.log(prevFormData);
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
   }
 
-  // steps to do after the submit button is been clicked
-  function submitHandler(event){
+  function submitHandler(event)
+  {
     event.preventDefault();
-    // this prevents the tasks that is done by default by clicking the submit button
-
-    console.log("Finally printing the value of Form Data:");
+    console.log("Form Give DAta")
     console.log(formData);
   }
 
-  
   return (
-    <div className='flex flex-col items-center mt-2'>
-      <h1>Form APP</h1>
+    <div className="App">
       <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Enter First name"
+            // onChange={changeFirstNameHandler}
+            onChange={changeHandler}
+            name="firstName"
+            value={formData.firstName}
+          />
 
-        <label htmlFor='firstName'>First Name</label>
-        <br/>
-        <input type='text' name="firstName" id="firstName" placeholder='Parth' value={formData.firstName} onChange={changeHandler} className='outline'/>
-        <br/>
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Enter Last name"
+            // onChange={changeLastNameHandler}
+            onChange={changeHandler}
+            name="lastName"
+            value={formData.lastName}
+          />
 
-        <label htmlFor='lastName'>Last Name</label>
-        <br/>
-        <input type='text' name="lastName" id='lastName' placeholder='Madhvani' value={formData.lastName} onChange={changeHandler} className='outline'/>
-        <br/>
+          <br />
+          <br />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            onChange={changeHandler}
+            name="email"
+            value={formData.email}
+          />
 
-        <label htmlFor='email'>Email address</label>
-        <br/>
-        <input type='email' name="email" id='email' placeholder='parth@abcd.com' value={formData.email} onChange={changeHandler} className='outline'/>
-        <br/>
+          <br />
+          <br />
+          <textarea
+            name="comment"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Enter your Comments"
+            onChange={changeHandler}
+            value={formData.value}
+          ></textarea>
 
-        <label htmlFor='country'>Country</label>
-        <br/>
-        <select id='country' name='country' value={formData.country} onChange={changeHandler} className='outline'>
-          
-          <option>India</option>
-          <option>United States</option>
-          <option>Canada</option>
-          <option>Mexico</option>
-        
-        </select>
-        <br/>
+          <br />
+          <br />
+          <input
+            type="checkbox"
+            name="isAgree"
+            onChange={changeHandler}
+            id="isAgree"
+            checked={formData.isAgree}
+          />
 
-        <label htmlFor='streetAddress'>Street address</label>
-        <br/>
-        <input type='text' name="streetAddress" id='streetAddress' placeholder='1234 Main St' value={formData.streetAddress} onChange={changeHandler} className='outline'/>
-        <br/>
+          <label htmlFor="isAgree">Agree Term and Condition</label>
 
-        <label htmlFor='city'>City</label>
-        <br/>
-        <input type='text' name="city" id='city' placeholder='Surat' value={formData.city} onChange={changeHandler} className='outline'/>
-        <br/>
+          <br />
+          <br />
 
-        <label htmlFor='state'>State / Province</label>
-        <br/>
-        <input type='text' name="state" id='state' placeholder='Gujarat' value={formData.state} onChange={changeHandler} className='outline'/>
-        <br/>
+          <fieldset> 
 
-        <label htmlFor='postalCode'>ZIP / Postal Code</label>
-        <br/>
-        <input type='text' name="postalCode" id='postalCode' placeholder='395009' value={formData.postalCode} onChange={changeHandler} className='outline'/>
-        <br/>
+          <input
+            type="radio"
+            name="mode"
+            id="Dark-mode"
+            value="Dark-mode"
+            onChange={changeHandler}
+            checked={formData.mode === "Dark-mode"}
+          />
 
-        <br/>
-        <fieldset>
+          <label htmlFor="Dark-mode">Dark Mode</label>
 
-        <legend>By Email</legend>
-        <br/>
-        <input type="checkbox"/>
-        <label>Comments</label>
-        <p>Get notified when someones posts a comment on a posting.</p>
-        <br/>
-        <input type="checkbox"/>
-        <label>Candidates</label>
-        <p>Get notified when a candidate applies for a job.</p>
-        <br/>
-        <input type="checkbox"/>
-        <label>Offers</label>
-        <p>Get notified when a candidate accepts or rejects an offer.</p>
+          <input
+            type="radio"
+            name="mode"
+            id="Light-mode"
+            value="Light-mode"
+            onChange={changeHandler}
+            checked={formData.mode === "Light-mode"}
+          />
+          <label htmlFor="Light-mode">Light Mode</label>
         </fieldset>
-        <label>Push Notifications</label>
-        <p>These are delivered via SMS to your mobile phone.</p>
-        <br/>
-        <br/>
 
+        <select
+          name="favCar"
+          id="favCar"
+          value={formData.favCar}
+          onChange={changeHandler}
+        >
+          <option value="scarpio">Scarpio</option>
+          <option value="russian">Russian</option>
+          <option value="thar">Thar</option>
+          <option value="bmw">BMW</option>
+          <option value="other" selected>Other</option>
+        </select>
+
+        <label htmlFor="favCar">Your Favrioute Car</label>
+
+        <br /><br />
+
+        <input type="submit" value="Submit" />
       </form>
     </div>
   );
 }
-
 
 export default App;
